4aded07a9aa945d3e75b003f2a20e9cd57e12c25
8CD <mail@8cd.ru>
2016-11-12 15:29:35 +0300

    init

---
+++ hello.py
@@ -0,0 +1,6 @@
+def application(env, start_response):
+    data = str(env['QUERY_STRING']).replace('&', '\n')
+    start_response("200 OK", [
+        ("Content-Type", "text/plain")
+    ])
+    return [data]
